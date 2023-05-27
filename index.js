1ca737078dfcc5cff4b36a3e1e253dbe171cabd3
souravadhikar1 <souravadhikari09296@gmail.com>
2023-05-28 00:08:51 +0600

    data added

--- index.js
+++ index.js
@@ -26,9 +26,14 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroDB").collection("menu");
+    const reviewCollection = client.db("bistroDB").collection("reviews");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
       res.send(result);
     });
 
